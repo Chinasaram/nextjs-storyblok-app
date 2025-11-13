3a26f0f5cefc9ab3b97170a8b9b977decda1b575
Chinasaram <anelechinasaram@gmail.com>
2025-11-13 22:46:04 +0000

    added symbol to feature.jsx

--- components/Feature.jsx
+++ components/Feature.jsx
@@ -3,7 +3,12 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 export default function Feature({ blok }) {
   return (
     <div className="feature" {...storyblokEditable(blok)}>
-      <span>{blok.name}</span>
+      {blok.symbol && (
+        <div className="feature-symbol">
+          <span className="text-4xl">{blok.symbol}</span>
+        </div>
+      )}
+      <h3 className="feature-name">{blok.name}</h3>
     </div>
   );
 }
